abe1bc54d745054f16aa88be95b29bd236976253
Pranav Bale <pranavbale99@gmail.com>
2024-02-15 01:20:15 +0530

    spring security 6

--- BasicSecurityProject/BasicSecurity /src/main/java/com/pranavbale/security/controller/PersonController.java
+++ BasicSecurityProject/BasicSecurity /src/main/java/com/pranavbale/security/controller/PersonController.java
@@ -1,7 +1,9 @@
 package com.pranavbale.security.controller;
 
 import com.pranavbale.security.entity.PersonData;
+import com.pranavbale.security.entity.UserInfo;
 import com.pranavbale.security.service.PersonService;
+import com.pranavbale.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +17,18 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Hello World";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(@RequestBody UserInfo userInfo) {
+        userService.createUser(userInfo);
+        return "User Create Successfully";
     }
 
     @PostMapping("/create")
